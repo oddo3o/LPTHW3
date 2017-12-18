4855afb486c3b267e203dad9f3bfbc526280684d
Christian <chrgeneral@gmail.com>
2017-12-17 18:42:45 -0800

    Added Documentation

--- ex1.py
+++ ex1.py
@@ -3,3 +3,6 @@ print("Hello Again,")
 print("This is a bunch of testing.")
 print("I 'guess' all I am doing is printing.")
 print("TT^TT")
+
+# Mika wrote this
+print("But nobody came.")
--- ex18.py
+++ ex18.py
@@ -1,1 +1,25 @@
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+# This one is like your scripts with argv
 
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+# That *args is actually pointless, we can just do this
+
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+# This just takes one argument
+
+
+def print_none():
+    print("I got nothing")
+# This one takes no arguments
+
+
+print_two("Christian", "General")
+print_two_again("Christian", "General")
+print_one("First")
+print_none()
