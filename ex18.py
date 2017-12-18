7b973a780cb65f0598c2d697bb77a89b8bda629f
Christian <chrgeneral@gmail.com>
2017-12-17 19:05:52 -0800

    Completed study drills ex18

--- ex18.py
+++ ex18.py
@@ -1,10 +1,13 @@
 def print_two(*args):
+    # * pulls all arguments to the function into a list
+    # 'args' is being unpacked into 'arg1' and 'arg2'
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
 # This one is like your scripts with argv
 
 
 def print_two_again(arg1, arg2):
+    # 'arg' values in parentheses adds them to the function
     print(f"arg1: {arg1}, arg2: {arg2}")
 # That *args is actually pointless, we can just do this
 
@@ -20,6 +23,6 @@ def print_none():
 
 
 print_two("Christian", "General")
-print_two_again("Christian", "General")
+print_two_again("Hello", "World")
 print_one("First")
 print_none()
